44a230e012c2db7b7a0f5f7d42ff9be94d706cb9
Kris Sakarias <ksakarias@gmail.com>
2020-05-21 17:15:16 -0700

    create source list file for compiling

---
+++ sourcelist.cmake
@@ -0,0 +1,11 @@
+target_sources(chess PRIVATE
+  src/info.cpp 
+  src/board.cpp 
+  src/pawn.cpp 
+  src/knight.cpp 
+  src/king.cpp 
+  src/bishop.cpp 
+  src/rook.cpp 
+  src/queen.cpp 
+  src/game.cpp
+)
